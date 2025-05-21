58a5130731b9af7ceab89050f115e60b72bf32af
ANESGH7 <143135389+ANESGH7@users.noreply.github.com>
2025-05-20 19:39:41 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,48 +1,19 @@
 const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: 3000 });
+const wss = new WebSocket.Server({ port: 3000 });
 
-let clients = new Map();
+const clients = new Set();
 
-server.on('connection', (socket) => {
-  const id = Math.random().toString(36).substr(2, 9);
-  clients.set(id, socket);
-  console.log(`Client connected: ${id}`);
+wss.on('connection', ws => {
+  clients.add(ws);
 
-  socket.on('message', (message) => {
-    let data;
-    try {
-      data = JSON.parse(message);
-    } catch (e) {
-      console.error('Invalid JSON:', message);
-      return;
-    }
-
-    switch (data.type) {
-      case 'join':
-        for (let [peerId, peerSocket] of clients) {
-          if (peerId !== id) {
-            peerSocket.send(JSON.stringify({ type: 'new-peer', id }));
-            socket.send(JSON.stringify({ type: 'new-peer', id: peerId }));
-          }
-        }
-        break;
-      case 'signal':
-        const target = clients.get(data.target);
-        if (target) {
-          target.send(JSON.stringify({
-            type: 'signal',
-            from: id,
-            data: data.data
-          }));
-        }
-        break;
+  ws.on('message', msg => {
+    // Forward message to all other clients
+    for (let client of clients) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(msg);
+      }
     }
   });
 
-  socket.on('close', () => {
-    clients.delete(id);
-    for (let peerSocket of clients.values()) {
-      peerSocket.send(JSON.stringify({ type: 'peer-disconnect', id }));
-    }
-  });
+  ws.on('close', () => clients.delete(ws));
 });
