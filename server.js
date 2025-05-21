04ef1dfc5bcc157ffe8c32889a439acb262712dc
ANESGH7 <143135389+ANESGH7@users.noreply.github.com>
2025-05-20 22:28:38 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-// server.js
+// signaling-server.js
 import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', (ws) => {
-  ws.on('message', (msg) => {
-    // Broadcast to everyone except sender
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    // Broadcast signaling messages to other clients
     wss.clients.forEach(client => {
       if (client !== ws && client.readyState === client.OPEN) {
-        client.send(msg);
+        client.send(message);
       }
     });
   });
-  ws.on('close', () => console.log('Client disconnected'));
 });
 
-console.log('WebSocket relay server running on ws://localhost:8080');
+console.log('Signaling server running on ws://localhost:8080');
