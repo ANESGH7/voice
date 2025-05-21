25009d43540d75f81269b86f6850dae336ec2a0a
ANESGH7 <143135389+ANESGH7@users.noreply.github.com>
2025-05-20 22:11:54 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
+// server.js
 import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
+  ws.on('message', (msg) => {
     // Broadcast to everyone except sender
     wss.clients.forEach(client => {
       if (client !== ws && client.readyState === client.OPEN) {
-        client.send(message);
+        client.send(msg);
       }
     });
   });
-
-  ws.on('close', () => {
-    console.log('Client disconnected');
-  });
+  ws.on('close', () => console.log('Client disconnected'));
 });
 
 console.log('WebSocket relay server running on ws://localhost:8080');
